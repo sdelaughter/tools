122b6281ee11ecf836c7be0303dc2b182e87d7ff
Sam DeLaughter <sdelaughter@users.noreply.github.com>
2026-08-13 11:19:58 +0200

    Update dotpng.sh

--- dotpng.sh
+++ dotpng.sh
@@ -2,6 +2,6 @@
 
 # Create a .png image from a .dot (graphviz) image
 
-in_file=$1
-filename=$(echo $in_file | cut -d "." -f 1)
-dot -Tpng $in_file >$filename.png
+infile=$1
+filename=$(echo $infile | cut -d "." -f 1)
+dot -Tpng $infile >$filename.png
